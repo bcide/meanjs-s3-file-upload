f26c285a6eb524d0ba6d813543d8def5cf439dad
Shama Hoque <shama.hoque@gmail.com>
2015-07-06 13:00:56 -0700

    production.js updated

--- config/env/production.js
+++ config/env/production.js
@@ -29,7 +29,8 @@ module.exports = {
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/ng-file-upload/angular-file-upload.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
